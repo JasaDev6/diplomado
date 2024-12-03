a0e4fb5edce467651d4548e866d16b85b3c56925
JasaDev6 <sabathdev@gmail.com>
2024-12-03 13:17:33 -0400

    add ssl test

--- src/database/database.js
+++ src/database/database.js
@@ -19,16 +19,4 @@ const sequelize = new Sequelize(
     }
 );
 
-// Test database connection
-const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-};
-
-testConnection();
-
 export default sequelize;
